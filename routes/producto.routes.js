24496f1619f79263c86c121258162e1c108d40d6
Jhoiner <jjmartinezs@ufpso.edu.co>
2024-04-10 22:18:03 -0500

    Commit inicial

---
+++ routes/producto.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import ProductoController from "../controllers/producto.controller.js"
+
+const routeProducto = Router()
+
+routeProducto.get("/", ProductoController.getProducto)
+routeProducto.post("/", ProductoController.postProducto)
+
+export default routeProducto
